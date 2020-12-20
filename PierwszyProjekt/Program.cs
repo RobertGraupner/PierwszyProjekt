e5ee5d8d8cb55461808f37ce07647a48fec6714e
Robert Graupner <robert.graupnerr@gmail.com>
2020-12-20 15:26:16 +0100

    initial commit

---
+++ PierwszyProjekt/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace PierwszyProjekt
+{
+
+    class Program
+    {
+        public static void Main()
+        {
+            var number1 = int.Parse(Console.ReadLine());
+            var number2 = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Odejmowanie: " + (number1 - number2));
+            Console.WriteLine("Dodawanie: " + (number1 + number2));
+            Console.WriteLine("Mnożenie: " + (number1 * number2));
+            Console.WriteLine("Dzielenie: " + (number1 / number2));
+        }
+    }
+    
+}
