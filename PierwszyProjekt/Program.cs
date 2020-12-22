2b9f8fd5e8522131f1def9bdd062c85f9a5ae02d
Robert Graupner <robert.graupnerr@gmail.com>
2020-12-22 13:49:00 +0100

    Add methods

--- PierwszyProjekt/Program.cs
+++ PierwszyProjekt/Program.cs
@@ -5,16 +5,52 @@ namespace PierwszyProjekt
 
     class Program
     {
-        public static void Main()
+         public static void Main()
         {
-            var number1 = int.Parse(Console.ReadLine());
-            var number2 = int.Parse(Console.ReadLine());
 
-            Console.WriteLine("Odejmowanie: " + (number1 - number2));
-            Console.WriteLine("Dodawanie: " + (number1 + number2));
-            Console.WriteLine("Mnożenie: " + (number1 * number2));
-            Console.WriteLine("Dzielenie: " + (number1 / number2));
+            bool ifNumber1 = int.TryParse(Console.ReadLine(), out int number1);
+            
+            if (!ifNumber1)
+            {
+                Console.WriteLine("You must enter a number!");
+                number1 = int.Parse(Console.ReadLine());
+            }
+
+            bool ifNumber2 = int.TryParse(Console.ReadLine(), out int number2);
+
+            if (!ifNumber2)
+            {
+                Console.WriteLine("You must enter a number!");
+                number2 = int.Parse(Console.ReadLine());
+            }
+
+            static void SubFunction(int number1 , int number2)
+            {
+                Console.WriteLine("Substraction: " + (number1 - number2));
+            }
+
+            static void AddFunction(int number1, int number2)
+            {
+                Console.WriteLine("Adding: " + (number1 + number2));
+            }
+
+            static void MultiFunction(int number1, int number2)
+            {
+                Console.WriteLine("Multiply: " + (number1 * number2));
+            }
+
+            static void DivFunction(int number1, int number2)
+            {
+                Console.WriteLine("Dividing: " + (number1 / number2));
+            }
+
+            SubFunction(number1 , number2);
+            AddFunction(number1 , number2);
+            MultiFunction(number1 , number2);
+            DivFunction(number1 , number2);
+          
         }
     }
+
     
 }
